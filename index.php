c1c732e2c93fef23cb94397db25c8dd451b3fa0e
Kyan Azaizeh <kyanazaizeh@Kyans-MacBook-Pro-2.local>
2023-10-26 20:29:10 +0300

    Added master Select bar

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+  <head>
+    <title>Home page</title>
+    <link rel="stylesheet" type="text/css" href="Styles/indexStyle.css" />
+  </head>
+  <body>
+  <iframe src="master.php" frameborder="0" width="100%" height="50"></iframe>
+    <h1 class="h1-index">Project Side Quest</h1>
+
+    <div class="textProf-container">
+      <img src="images/Av-Def.png" class="avatar-image" alt="Avatar-Deafult" />
+      <h2>User_name</h2>
+      <p>fname lname</p>
+      <p>bio: Refining project SideQuest.</p>
+    </div>
+    <hr
+      style="color: #333; background-color: #333; height: 10px; border: none"
+    />
+    <div class="text-container">
+      <pre>
+        <p><a href="Calculator.php"><img src="images/Calculator.icon.png" class="avatar-image"/></a></p>
+      </pre>
+    </div>
+    </body>
+
+    <div class="text-container">
+      <pre>
+        <p><a href="regForm.php"><img src="images/registry.icon.png" class="avatar-image"/></a></p>
+      </pre>
+    </div>
+
+    <div class="text-container">
+      <pre>
+        <p><a href="phpHandlers/users.php"><img src="images/debug.icon.png" class="avatar-image"/></a></p>
+      </pre>
+    </div>
+
+    <div class="text-container">
+      <pre>
+        <p><a href="form.php"><img src="images/form.icon.png" class="avatar-image"/></a></p>
+      </pre>
+    </div>
+
+    <div class="text-container">
+      <pre>
+        <p><a href="login.php"><img src="images/login.icon.jpeg" class="avatar-image"/></a></p>
+      </pre>
+    </div>
+
+    <div class="text-container">
+      <pre>
+        <p><a href="admin-panel.php"><img src="images/admin.icon.png" class="avatar-image"/></a></p>
+      </pre>
+    </div>
+  </body>
+</html>
